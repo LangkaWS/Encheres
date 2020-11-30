e1fcb4d160d252036fcb91e5d77b8cf8f8cd98ea
Heroponne <55738457+Heroponne@users.noreply.github.com>
2020-11-30 21:14:51 +0100

    [BACK] edited servlets for log in and log out

--- src/fr/eni/encheres/ihm/servlets/ServletSignIn.java
+++ src/fr/eni/encheres/ihm/servlets/ServletSignIn.java
@@ -8,6 +8,12 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import fr.eni.encheres.bll.BLLException;
+import fr.eni.encheres.bll.ManagerFactory;
+import fr.eni.encheres.bll.UserManager;
+import fr.eni.encheres.bll.bo.User;
 
 /**
  * Servlet implementation class ServletSignIn
@@ -15,14 +21,37 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/ServletSignIn")
 public class ServletSignIn extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private UserManager um = ManagerFactory.getUserManager();
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String email = request.getParameter("emailInput");
+		String password = request.getParameter("passwordInput");
 		
-		RequestDispatcher rd = request.getRequestDispatcher("/index.jsp");
-		rd.forward(request, response);
+		HttpSession session = request.getSession();
+		
+		try {
+			User user = um.getUser(email);
+			if (user == null) {
+				throw new BLLException("Error - User seems to not exist.");
+			} else if (!user.getPassword().equals(password)) {
+				throw new BLLException("Error - Wrong password.");
+			} else {
+				session.setAttribute("currentUser", user);
+				
+				RequestDispatcher rd = request.getRequestDispatcher("/index.jsp");
+				rd.forward(request, response);
+			}
+		} catch (BLLException e) {
+			e.printStackTrace();
+			request.setAttribute("exception", e);
+			request.setAttribute("email", email);
+			request.setAttribute("password", password);
+			RequestDispatcher rd = request.getRequestDispatcher("/signIn.jsp");
+			rd.forward(request, response);
+		}
 	}
 
 	/**
