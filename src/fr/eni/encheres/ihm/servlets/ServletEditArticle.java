0737c83f23d41d0ca2a20d09ff0f392b7be7e2e0
LangkaWS <60548795+LangkaWS@users.noreply.github.com>
2020-12-03 14:09:11 +0100

    Create edit auction functionality /!\ BUG see comment
    Error in updating article occuring in ArticleManager.
    fr.eni.encheres.bll.BLLException: Business Exception - An error occured in business layer.Inserting article failed, article already exists.
        at fr.eni.encheres.bll.ArticleManager.validateArticle(ArticleManager.java:180)
        at fr.eni.encheres.bll.ArticleManager.updateArticle(ArticleManager.java:44)
        at fr.eni.encheres.ihm.servlets.ServletEditArticle.doPost(ServletEditArticle.java:82)

---
+++ src/fr/eni/encheres/ihm/servlets/ServletEditArticle.java
@@ -0,0 +1,91 @@
+package fr.eni.encheres.ihm.servlets;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import fr.eni.encheres.bll.ArticleManager;
+import fr.eni.encheres.bll.BLLException;
+import fr.eni.encheres.bll.PickUpManager;
+import fr.eni.encheres.bll.bo.Article;
+import fr.eni.encheres.bll.bo.PickUp;
+import fr.eni.encheres.bll.bo.User;
+
+/**
+ * Servlet implementation class ServletEditArticle
+ */
+@WebServlet("/edit/auctionDetail")
+public class ServletEditArticle extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	ArticleManager am = new ArticleManager();
+	PickUpManager pm = new PickUpManager();
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		Integer articleId = Integer.parseInt(request.getParameter("id"));
+		System.out.println(articleId);
+		
+		Article art = null;
+		
+		try {
+			art = am.getArticleById(articleId);
+		} catch (BLLException e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println(art.toString());
+		
+		request.setAttribute("art", art);
+		
+		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/editAuction.jsp");
+		rd.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		request.setCharacterEncoding("UTF-8");
+		
+		Integer articleId = Integer.parseInt(request.getParameter("id"));
+		String articleName = request.getParameter("articleName");
+		String articleDescription = request.getParameter("articleDescription");
+		Integer category = Integer.parseInt(request.getParameter("category"));
+		Integer articleStartPrice = Integer.parseInt(request.getParameter("articleStartPrice"));
+		LocalDateTime startDate = LocalDateTime.parse(request.getParameter("startDate"));
+		LocalDateTime endDate = LocalDateTime.parse(request.getParameter("endDate"));
+		String street = request.getParameter("street");
+		String zipCode = request.getParameter("zipCode");
+		String town = request.getParameter("town");
+		
+		Integer sellerId = ((User) request.getSession().getAttribute("currentUser")).getUserId();
+		
+		PickUp pickUp = new PickUp(street, zipCode, town);
+		
+		Article article = new Article(articleName, articleDescription, startDate, endDate, articleStartPrice, "created", sellerId, category, 0);
+		article.setArticleId(articleId);
+		
+		try {
+			pm.addPickUp(pickUp);
+			article.setPickUpId(pickUp.getPickUpId());
+			am.updateArticle(article);
+		} catch (BLLException e) {
+			e.printStackTrace();
+		}
+		
+		response.sendRedirect(request.getContextPath() + "/auctionDetail?id=" + articleId);
+		
+	}
+
+}
